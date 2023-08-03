e7fe3c6277a9e2c661a162ae351a00ad9490298b
Mike Legend <michael.george.why@gmail.com>
2023-08-03 15:23:35 -0700

    initialize

--- app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
+++ app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //hello all
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
