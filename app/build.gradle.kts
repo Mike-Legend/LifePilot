31d2582912f48d3e20ac288ec9331bee618afe29
Malik Bailey <MYBailey@student.fullsail.edu>
2023-08-14 22:01:00 -0400

    Progress on exercise data and added graphview to gradle dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.jjoe64.:graphview:4.2.2")
 }
