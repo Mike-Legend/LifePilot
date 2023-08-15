861fb99c8972c5cfbcce4c666ce9740efcaf6922
Malik Bailey <MYBailey@student.fullsail.edu>
2023-08-14 22:27:19 -0400

    Removed gridview from gradle to figure out at later time.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.jjoe64.:graphview:4.2.2")
 }
