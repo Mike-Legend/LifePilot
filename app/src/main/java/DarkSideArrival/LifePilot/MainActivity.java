3ab67fd32a45f3af25fcaff3c3857aa4478725c3
Mike Legend <michael.george.why@gmail.com>
2023-08-16 17:02:16 -0700

    Routine edits

--- app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
+++ app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
@@ -2,15 +2,40 @@ package DarkSideArrival.LifePilot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //hello all
-
-    @Override
+    @Override //Initial App Generation
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override //Used for on click section in layout button attribute to switch layouts.
+    public void onClick(View view) //add button with an else-if statement
+    {
+        //TODO: animations work in progress
+        Animation SlideLeftIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
+        Animation SlideRightIn = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        Animation SlideLeftOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
+        Animation SlideRightOut = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
+
+        int id = view.getId();
+        if(id == R.id.Goal_Button) {
+            view.startAnimation(SlideLeftIn);
+            setContentView(R.layout.routine_goals);
+        } else if (id == R.id.GoalBack_Button) {
+            setContentView(R.layout.routine_list);
+        } else if (id == R.id.routinegotobutton) {
+            setContentView(R.layout.routine_list);
+        }
     }
 }
