221fd7c0dd814794b65bd936bbd0d93b481af4c5
Demetrius Henry <DBHenry@student.fullsail.edu>
2023-09-13 23:46:45 -0400

    New Demetrius branch

--- app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
+++ app/src/main/java/DarkSideArrival/LifePilot/MainActivity.java
@@ -3,8 +3,10 @@ package DarkSideArrival.LifePilot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     //hello all
 
@@ -12,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
