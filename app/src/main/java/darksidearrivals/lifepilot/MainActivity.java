495a8121b13ca74d9edf17de4e1ddff83dd13cd1
Mike Legend <michael.george.why@gmail.com>
2023-09-20 07:43:06 -0700

    merge updates

--- app/src/main/java/darksidearrivals/lifepilot/MainActivity.java
+++ app/src/main/java/darksidearrivals/lifepilot/MainActivity.java
@@ -229,7 +229,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-
                 editChecker = 1;
                 goalAnimation = Scene.getSceneForLayout(findViewById(R.id.TransitionRoutineLayout), R.layout.routine_goals, this);
                 homeAnimation = Scene.getSceneForLayout(findViewById(R.id.TransitionRoutineLayout), R.layout.activity_main, this);
